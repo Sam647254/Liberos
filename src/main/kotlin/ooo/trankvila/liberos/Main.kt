d9c27e0574ed080f69f49f640ccd2d1e1b51677d
Sam Wang <samofcorinth@gmx.ca>
2021-03-07 21:01:22 -0800

    Finis la bazan I/O

--- src/main/kotlin/ooo/trankvila/liberos/Main.kt
+++ src/main/kotlin/ooo/trankvila/liberos/Main.kt
@@ -1,5 +1,14 @@
 package ooo.trankvila.liberos
 
-fun main(args: Array<String>) {
+import java.io.File
+import kotlin.system.exitProcess
 
+fun main(args: Array<String>) {
+   if (args.isEmpty()) {
+      println("Argument: [input file]")
+      exitProcess(-1)
+   }
+   val enhavo = File(args[0]).readText()
+   val rakonto = Rakonto(enhavo)
+   rakonto.komenci(System.`in`, System.out)
 }
--- src/main/kotlin/ooo/trankvila/liberos/Rakonto.kt
+++ src/main/kotlin/ooo/trankvila/liberos/Rakonto.kt
@@ -6,6 +6,7 @@ import java.lang.RuntimeException
 
 class Rakonto(eniro: String) {
    private val partoj: List<Parto>
+   private val multefojaVortoj = mutableMapOf<String, String>()
 
    init {
       val partoj = mutableListOf<Parto>()
@@ -26,14 +27,19 @@ class Rakonto(eniro: String) {
                   throw RuntimeException("'[' ne trovita")
                }
                partoj.add(UnufojaVorto(eniro.substring(lastaKomenco, i)))
+               lastaBordo = null
+               lastaKomenco = i + 1
             }
             '}' -> {
                if (lastaBordo != '{') {
                   throw RuntimeException("'{' ne trovita")
                }
                partoj.add(MultefojaVorto(eniro.substring(lastaKomenco, i)))
+               lastaBordo = null
+               lastaKomenco = i + 1
             }
-            else -> { }
+            else -> {
+            }
          }
       }
       this.partoj = partoj
@@ -41,22 +47,49 @@ class Rakonto(eniro: String) {
 
    fun komenci(eniro: InputStream, eliro: OutputStream) {
       val vortoj = partoj.filter { it is UnufojaVorto || it is MultefojaVorto }
-      val elirilo = eliro.writer()
+      val elirilo = eliro.bufferedWriter()
       val legilo = eniro.bufferedReader()
 
       vortoj.forEachIndexed { index, vorto ->
+         when (vorto) {
+            is UnufojaVorto -> {
+               elirilo.write("[${index + 1}/${vortoj.size}] $vorto: ")
+               elirilo.flush()
+               val eniraVorto = legilo.readLine()
+               vorto.eniro = eniraVorto
+            }
+            is MultefojaVorto -> {
+               if (!multefojaVortoj.containsKey(vorto.nomo)) {
+                  elirilo.write("[${index + 1}/${vortoj.size}] $vorto: ")
+                  elirilo.flush()
+                  val eniraVorto = legilo.readLine()
+                  multefojaVortoj[vorto.nomo] = eniraVorto
+               }
+               vorto.eniro = multefojaVortoj[vorto.nomo]
+            }
+            else -> {
+            }
+         }
       }
+
+      partoj.map(Parto::toString).forEach(::print)
    }
 }
 
 sealed class Parto
 
-data class Teksto(val teksto: String) : Parto()
-
-data class UnufojaVorto(val tipo: String) : Parto() {
-   override fun toString(): String {
-      return tipo
-   }
+data class Teksto(val teksto: String) : Parto() {
+   override fun toString() = teksto
 }
 
-data class MultefojaVorto(val nomo: String) : Parto()
+data class UnufojaVorto(val tipo: String) : Parto() {
+   var eniro: String? = null
+
+   override fun toString() = eniro?.let { "[$it]" } ?: tipo
+}
+
+data class MultefojaVorto(val nomo: String) : Parto() {
+   var eniro: String? = null
+
+   override fun toString() = eniro?.let { "{$it}" } ?: nomo
+}
